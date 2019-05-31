03b72c75ab89d1382b0f6c413c3afdba8ff72022
卞辉 <bianhui0524@sina.com>
2019-05-31 15:55:24 +0800

    初始化代码

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PYLINGQ',
+    version=0.1,
+    description=(
+        '和c#一样使用方式的linq',
+        'the linq like c#'
+    ),
+    long_description=open('README.rst','rb').read(),
+    author='卞辉（beincy）',
+    author_email='bianhui0524@sina.com',
+    maintainer='卞辉(beincy)',
+    maintainer_email='bianhui0524@sina.com',
+    license='MIT',
+    packages=['PYLINQ'],
+    platforms=["all"],
+    url='https://github.com/beincy/PYLINQ',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
