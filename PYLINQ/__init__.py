1056c6e2363d16642aa8d4303b6fdf8663d28f41
卞辉 <bianhui0524@sina.com>
2019-06-03 15:37:31 +0800

    新增若干方法

--- PYLINQ/__init__.py
+++ PYLINQ/__init__.py
@@ -1,5 +1,9 @@
+from operator import itemgetter, attrgetter
+
+
 class PYLINQ:
     _inerList = None
+
     def __init__(self, array: list):
         self._inerList = array
 
@@ -9,7 +13,6 @@ class PYLINQ:
         '''
         for item in self._inerList:
             yield item
-
 
     def toList(self):
         '''
@@ -103,6 +106,61 @@ class PYLINQ:
         '''
         return OrderIterator(self, func, True)
 
+    def groupBy(self, func=lambda x: x):
+        '''
+        order by desc
+        '''
+        return GroupByIterator(self, func)
+
+    def sum(self, func=lambda x: x):
+        '''
+        sum 
+        '''
+        total = 0
+        for item in self:
+            total += func(item)
+        return total
+
+    def skip(self, size=0):
+        '''
+        skip:
+        size:
+        '''
+        return SkipIterator(self, size)
+
+    def max(self, func=lambda x: x):
+        '''
+        max of item
+        '''
+        maxModel=None
+        for item in self:
+            if maxModel is None:
+                maxModel=item
+            elif func(item) >func(maxModel):
+                maxModel=item
+        return maxModel
+
+    def min(self, func=lambda x: x):
+        '''
+        max of item
+        '''
+        maxModel=None
+        for item in self:
+            if maxModel is None:
+                maxModel=item
+            elif func(item) < func(maxModel):
+                maxModel=item
+        return maxModel
+
+    def forEach(self, func=lambda x: x):
+        '''
+        Cyclic processing of each element
+        '''
+        def _calculation():
+            for item in self:
+                yield func(item)
+        return PYLINQ(_calculation())
+
 
 class WhereIterator(PYLINQ):
     '''
@@ -138,6 +196,7 @@ class SelectIterator(PYLINQ):
         for item in self._inerList:
             yield self._func(item)
 
+
 class TakeIterator(PYLINQ):
     '''
     Gets a specified number of items
@@ -157,6 +216,27 @@ class TakeIterator(PYLINQ):
                 yield item
             else:
                 break
+
+
+class SkipIterator(PYLINQ):
+    '''
+    Skip a specified number of items
+    '''
+
+    def __init__(self, array: list, count=1):
+        super(SkipIterator, self).__init__(array)
+        self._count = count
+
+    def __iter__(self):
+        '''
+        Generating Iterative Method
+        '''
+        for item in self._inerList:
+            if self._count > 0:
+                self._count -= 1
+                continue
+            else:
+                yield item
 
 
 class RemoveIterator(PYLINQ):
@@ -189,6 +269,100 @@ class OrderIterator(PYLINQ):
     sort list
     '''
 
-    def __init__(self, array: list, func=lambda x: x, reverse=False):
+    def __init__(self, array: list, keyFunc=lambda x: x, reverse=False):
         super(OrderIterator, self).__init__(array)
-        self._inerList = sorted(self._inerList, func, reverse)
+        self._keyFunc = keyFunc
+        self._keyFuncs = [keyFunc]
+        self._reverse = reverse
+
+    def __iter__(self):
+        '''
+        sort
+        '''
+        tmpList = []
+        if len(self._keyFuncs) > 1:
+            def orderKey(keyFuncs, model):
+                for kf in keyFuncs:
+                    yield kf(model)
+            tmpList = sorted(self._inerList, key=lambda x: tuple([x for x in orderKey(self._keyFuncs, x)]),
+                             reverse=self._reverse)
+        else:
+            tmpList = sorted(self._inerList, key=self._keyFunc,
+                             reverse=self._reverse)
+        for item in tmpList:
+            yield item
+
+    def thenBy(self, keyFunc=lambda x: x):
+        '''
+        the next order by
+        '''
+        def orderFunc(model):
+            v = keyFunc(model)
+            if self._reverse and (isinstance(v, int) or isinstance(v, float)):
+                return -v
+            return v
+        self._keyFuncs.append(orderFunc)
+        return self
+
+    def thenByDesc(self, keyFunc=lambda x: x):
+        '''
+        the next order by desc
+        '''
+        def descOrderFunc(model):
+            v = keyFunc(model)
+            if not self._reverse and (isinstance(v, int) or isinstance(v, float)):
+                return -v
+            return v
+        self._keyFuncs.append(descOrderFunc)
+        return self
+
+
+class GroupByIterator():
+    grdoupCollection = {}
+    inerList = None
+
+    def __init__(self, array: list, keyFunc=lambda x: x):
+        self.inerList = array
+        self.keyFunc = keyFunc
+
+    def __iter__(self):
+        for target in self.inerList:
+            iremDir = {}
+            key = self.keyFunc(target)
+            if key not in iremDir:
+                iremDir[key] = True
+                yield GroupByItemIterator(key, self)
+
+
+class GroupByItemIterator():
+    key = None
+
+    def __init__(self, key, groupModel):
+        self.key = key
+        self._groupModel = groupModel
+
+    def __iter__(self):
+        if self.key in self._groupModel.grdoupCollection:
+            # 如果所在key没有，代表此数组从未循环过
+            for target in self._groupModel.grdoupCollection[self.key]:
+                yield target
+        else:
+            yield from self._forEachGroupItem()
+
+    @property
+    def value(self):
+        if self.key in self._groupModel.grdoupCollection:
+            return PYLINQ(self._groupModel.grdoupCollection[self.key])
+        else:
+            return PYLINQ(self._forEachGroupItem())
+
+    def _forEachGroupItem(self):
+        for item in self._groupModel.inerList:
+            itemKey = self._groupModel.keyFunc(item)
+            if itemKey not in self._groupModel.grdoupCollection:
+                self._groupModel.grdoupCollection[itemKey] = [item]
+            else:
+                self._groupModel.grdoupCollection[itemKey].append(item)
+
+            if self.key == itemKey:
+                yield item
